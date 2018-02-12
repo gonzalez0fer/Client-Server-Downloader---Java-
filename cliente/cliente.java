8fdecdb63c4dfb2e1c8e3218c7aa0b0e30b9f08c
Fernando Gonzalez <gonzalez0fer@gmail.com>
2018-02-11 22:15:54 -0400

    menu + comunicaciones + lista de archivos

--- cliente/cliente.java
+++ cliente/cliente.java
@@ -11,20 +11,50 @@ public class cliente{
   PrintStream salida;
 
   public void iniciar(){
-    try{
-     cliente = new Socket(ip, puerto);
-     entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
-     teclado = new BufferedReader(new InputStreamReader(System.in));
-     String tec = teclado.readLine();
-     salida = new PrintStream(cliente.getOutputStream());
-     salida.println(tec);
-     String msg = entrada.readLine();
-     System.out.println(msg);
+    System.out.println("Bienvenido usuario, desea conectarse? s/n");
+    Scanner scan = new Scanner(System.in);
+    String s = scan.next();
+    if (s.equals("s")){
 
-     entrada.close();
-     salida.close();
-     teclado.close();
-     cliente.close();
-   }catch(Exception e){};
+      try{
+       cliente = new Socket(ip, puerto);
+       System.out.println("Bienvenido usuario, para conectarse oprima a, para salir 0");
+       System.out.println("1) ESTADO_DESCARGAS");
+       System.out.println("2) LISTA_LIBROS");
+       System.out.println("3) SOLICITUD_LIBRO");
+       System.out.println("4) LIBROS_DESCARGADOSXSERVIDOR");
+       entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+       teclado = new BufferedReader(new InputStreamReader(System.in));
+       String tec = teclado.readLine();
+       salida = new PrintStream(cliente.getOutputStream());
+
+       if (tec.equals("1")) {
+         /*FUNCIONALIDAD DE ESTADO ACTUAL DE DESCARGAS*/
+         System.out.println("1) ESTADO_DESCARGAS");
+       }else if (tec.equals("2")) {
+         salida.println("LISTA_LIBROS");
+       }else if (tec.equals("3")) {
+         System.out.println("Cual libro desea descargar?");
+         String libro = scan.next();
+         salida.println(libro);
+
+       }else if (tec.equals("4")) {
+         /*FUNCIONALIDAD DE LIBROS DESCARGADOS*/
+         System.out.println("4) LIBROS_DESCARGADOSXSERVIDOR");
+
+       }
+
+
+       String msg = entrada.readLine();
+       System.out.println(msg);
+
+       entrada.close();
+       salida.close();
+       teclado.close();
+       cliente.close();
+    }catch(Exception e){};
+  } else {
+    System.out.println("Hasta luego");
+  }
  }
 }
--- servidor/servidor.java
+++ servidor/servidor.java
@@ -18,7 +18,21 @@ public class servidor{
 
       entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
       String mensaje = entrada.readLine();
+
+      if (mensaje.equals("LISTA_LIBROS")) {
+
+        String sDirectorio = "/home/fernando/Universidad/Redes/Cliente-Servidor/servidor/books";
+        File f = new File(sDirectorio);
+        File[] ficheros = f.listFiles();
+        for (int x=0;x<ficheros.length;x++){
+          System.out.println(ficheros[x].getName());
+        }
+
+      }
+
       System.out.println(mensaje);
+
+
       salida = new DataOutputStream(socket.getOutputStream());
       salida.writeUTF("recibido");
     }catch (Exception e){};
